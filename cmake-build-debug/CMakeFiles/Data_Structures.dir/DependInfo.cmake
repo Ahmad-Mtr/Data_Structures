07be4c06a0cbbd3740fe11304c08c55dca9aa916
Ahmad <ahmadmtr@protonmail.com>
2023-03-26 06:48:58 +0300

    0.2

---
+++ cmake-build-debug/CMakeFiles/Data_Structures.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/youse/CLionProjects/Data_Structures/LinkedLists/LinkedList_struct.cpp" "CMakeFiles/Data_Structures.dir/LinkedLists/LinkedList_struct.cpp.obj" "gcc" "CMakeFiles/Data_Structures.dir/LinkedLists/LinkedList_struct.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
